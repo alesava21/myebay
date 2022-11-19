5753ce4e69ff638de4041e8a884b593c4293ab11
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-11-19 21:34:25 +0100

    risoluzione problemi avvio applicazione

--- src/main/java/it/prova/myebay/security/CustomAuthenticationSuccessHandlerImpl.java
+++ src/main/java/it/prova/myebay/security/CustomAuthenticationSuccessHandlerImpl.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import it.prova.myebay.dto.UtenteDTO;
 import it.prova.myebay.model.Utente;
 import it.prova.myebay.repository.utente.UtenteRepository;
 
+@Component
 public class CustomAuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler{
 	
 	@Autowired
